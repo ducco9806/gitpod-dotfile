789ed8ccfdc0a1120e07ecf91af0a59d91b20bb3
ducco9806 <ducco9xpc@gmail.com>
2025-03-20 00:52:51 +0700

    add dotfiles gitpod

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+sudo ln -s ~/.dotfiles/.bash_aliases ~/.bash_aliases
+sudo ln -s ~/.dotfiles/.curlrc ~/.curlrc
+sudo ln -s ~/.dotfiles/.wgetrc ~/.wgetrc
+
+# Overwrite gitpod's existing config
+sudo rm ~/.gitconfig && sudo ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+
+# Setup essentials for development
+echo -e '\n[[ -s "$HOME/.dotfiles/.bash_prompt" ]] && source "$HOME/.dotfiles/.bash_prompt"' >> ~/.bashrc
+
+sudo cp -Rn ~/.dotfiles/.vscode $GITPOD_REPO_ROOT/.vscode
+sudo cp -n ~/.dotfiles/.editorconfig $GITPOD_REPO_ROOT
+sudo cp -n ~/.dotfiles/.gitignore $GITPOD_REPO_ROOT
+
+sudo chown -R gitpod:gitpod $GITPOD_REPO_ROOT/.vscode
+sudo chown gitpod:gitpod $GITPOD_REPO_ROOT/.editorconfig
+sudo chown gitpod:gitpod $GITPOD_REPO_ROOT/.gitignore
